6a033058072bcebf167038e42f839009f5b34d5d
Vixi22 <magickkaito@gmail.com>
2024-05-04 18:19:26 -0400

    added includes

--- src/main.c
+++ src/main.c
@@ -1,5 +1,13 @@
-#include <stdio.h>
+#include "raylib.h"
 
-int main(int argc, char **argv){
-  return 0;
+int main(){
+	int width = 800;
+	int height = 400;
+	InitWindow(width, height, "pi-chat");	
+	while(!WindowShouldClose()){
+		BeginDrawing();
+		ClearBackground(RAYWHITE);
+		EndDrawing();
+	}
+	return 0;
 }
